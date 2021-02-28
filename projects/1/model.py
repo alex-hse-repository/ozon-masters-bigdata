9004ba3a35e7964351f32dae322daaa81ef65a77
Alex <alex-box.ru@mail.ru>
2021-02-28 10:53:06 +0000

    1.joblib

--- projects/1/model.py
+++ projects/1/model.py
@@ -6,7 +6,7 @@ from sklearn.svm import LinearSVC
 
 numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
-categorical_features_train = ["cf"+str(i) for i in [6,13,17,19]] 
+categorical_features_train = ["cf"+str(i) for i in [6,17]] 
 features = numeric_features+categorical_features_train
 fields = ["id", "label"] + numeric_features + categorical_features
 
--- projects/1/predict.py
+++ projects/1/predict.py
@@ -20,7 +20,7 @@ model = load("1.joblib")
 
 numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
-categorical_features_train = ["cf"+str(i) for i in [6,13,17,19]] 
+categorical_features_train = ["cf"+str(i) for i in [6,17]] 
 features = numeric_features+categorical_features_train
 fields = ["id"] + numeric_features + categorical_features
 
