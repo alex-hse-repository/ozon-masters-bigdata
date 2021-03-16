772451fdfa4bdc26057469d02464b4d8c4272649
Alex <alex-box.ru@mail.ru>
2021-03-16 11:36:00 +0000

    1.joblib

--- projects/2/filter_predict.hql
+++ projects/2/filter_predict.hql
@@ -1,5 +1,7 @@
 ADD FILE projects/2/predict.py;
-INSERT INTO TABLE hw2_pred 
-select TRANSFORM(*) USING 'python3 predict.py' from 
-(select * from hw2_test
-where (f1 is not NULL) and (20 < f1 and f1 < 40)) as tmp;
+ADD FILE 2.joblib;
+INSERT INTO TABLE hw2_pred
+select TRANSFORM(*) USING '/opt/conda/envs/dsenv/bin/python predict.py' from 
+(select * from hw2_test_test
+where (if1 is not NULL) and (20 < if1 and if1 < 40)) as tmp
+limit 10;
--- projects/2/predict.py
+++ projects/2/predict.py
@@ -4,6 +4,7 @@ import sys, os
 import logging
 from joblib import load
 import pandas as pd
+import numpy as np
 
 sys.path.append('.')
 
@@ -21,15 +22,11 @@ model = load("2.joblib")
 numeric_features = ["if"+str(i) for i in range(1,14)]
 categorical_features = ["cf"+str(i) for i in range(1,27)] + ["day_number"]
 features = numeric_features
-fields = ["id"] + numeric_features + categorical_features
+fields = ["id"] + numeric_features+categorical_features
 
-#read and infere
-read_opts=dict(
-        sep='\t', names=fields, index_col=False,na_values='\\N', header=None,
-        iterator=True, chunksize=100
-)
-
-for df in pd.read_csv(sys.stdin, **read_opts):
-    pred = model.predict(df[features])
-    out = zip(df['id'], pred)
+for line in sys.stdin:
+    X = line.strip().split('\t')
+    X = pd.DataFrame([X],columns = fields).replace('\\N',np.NaN)
+    pred = model.predict(X[features])
+    out = zip(X['id'], pred)
     print("\n".join(["{0}\t{1}".format(*i) for i in out]))
